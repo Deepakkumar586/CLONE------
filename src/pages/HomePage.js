36e70a989b34b60d56927d1a31ccacd2622ab08e
Deepak <aryanrajj1234@gmail.com>
2023-05-09 19:03:30 +0530

    CredExperience/Security/brands file added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,11 @@ import HeroSection from '../components/HeroSection'
 import Header from '../components/common/header'
 import ProductShowcase from '../components/ProductShowcase'
 import FeelSpecial from '../components/FeelSpecial'
+import CredExperience from '../components/CredExperience'
+import CredSecurity from '../components/CredSecurity'
+import BrandsLove from '../components/BrandsLove'
+import WindowPeak from '../components/WindowPeak'
+import MobileScroll from '../components/MobileScroll'
 
 function HomePage() {
   return (
@@ -10,7 +15,17 @@ function HomePage() {
       <Header />
       <HeroSection />
       <ProductShowcase />
+
       <FeelSpecial />
+      <BrandsLove />
+      <CredExperience />
+      <MobileScroll />
+      <div className='non-mobile'>
+        <WindowPeak />
+      </div>
+
+      <CredSecurity />
+
     </>
   )
 }
